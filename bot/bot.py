254818b93c2f38a9f585f2817b65e3bd71857dd6
Lymar Volodymyr <shaxtar11@gmail.com>
2024-08-17 18:38:35 +0200

    Added saving info about user in database. Automatic language setting. Separate button for language change

--- bot/bot.py
+++ bot/bot.py
@@ -6,19 +6,21 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from dotenv import load_dotenv
-
+from database import create_database_connection, create_table_users, add_user, update_user_language
 
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
-
 API_TOKEN = os.getenv('BOT_TOKEN')
-
 
 bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
+
+# Connect to the database and create the table if it does not exist
+conn = create_database_connection()
+create_table_users(conn)
 
 # Storing the selected language
 user_language = {}
@@ -39,80 +41,93 @@ async def set_commands(bot: Bot):
 
 @dp.message(F.text == "/start")
 async def send_welcome(message: types.Message, state: FSMContext):
-    image_path = os.path.join(os.path.dirname(__file__), "images", 'welcome_image.jpg')
+    user = message.from_user
+    user_id = user.id
+    chat_id = message.chat.id
+    first_name = user.first_name
+    last_name = user.last_name
+    username = user.username
+    language_code = user.language_code
 
-    language_buttons = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="English", callback_data="lang_en"),
-            InlineKeyboardButton(text="Русский", callback_data="lang_ru")]
-    ])
+    # Language selection based on the user's language
+    if language_code in ["ru", "uk"]:
+        selected_language = "ru"
+    else:
+        selected_language = "en"
+
+    # Save user in DB
+    add_user(conn, chat_id, user_id, first_name, last_name, username, selected_language)
+
+    # Set the selected language
+    user_language[user_id] = selected_language
+
+    await show_game_selection(message, selected_language)
+
+
+# Language selection processing
+async def show_game_selection(message: types.Message, selected_language: str):
+    chat_id = message.chat.id
+    image_path = os.path.join(os.path.dirname(__file__), "images", 'start_image.jpg')
     if os.path.exists(image_path):
         photo = FSInputFile(image_path)
-        await message.answer_photo(photo=photo, caption="Выберите язык / Choose your language:", reply_markup=language_buttons)
-    else:
-        await message.answer("Выберите язык / Choose your language:", reply_markup=language_buttons)
-    await state.set_state(Form.choosing_language)
+        await bot.send_photo(chat_id, photo=photo)
+
+    buttons = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Riding Extreme 3D", callback_data="riding_extreme_3d"),
+         InlineKeyboardButton(text="Chain Cube 2048", callback_data="chain_cube_2048")],
+        [InlineKeyboardButton(text="My Clone Army", callback_data="my_clone_army"),
+         InlineKeyboardButton(text="Train Miner", callback_data="train_miner")],
+        [InlineKeyboardButton(text="Merge Away", callback_data="merge_away"),
+         InlineKeyboardButton(text="Twerk Race 3D", callback_data="twerk_race_3d")],
+    ])
+
+    await bot.send_message(
+        chat_id,
+        "Choose a game:" if selected_language == "en" else "Выберите игру:",
+        reply_markup=buttons
+    )
 
 
 # Change language button
 @dp.message(F.text == "/change_lang")
 async def change_language(message: types.Message, state: FSMContext):
     language_buttons = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="English", callback_data="lang_en"),
-            InlineKeyboardButton(text="Русский", callback_data="lang_ru")],
+        [InlineKeyboardButton(text="English", callback_data="en"),
+         InlineKeyboardButton(text="Русский", callback_data="ru")],
     ])
     await message.answer("Выберите язык / Choose your language:", reply_markup=language_buttons)
     await state.set_state(Form.choosing_language)
 
 
 # Language selection processing
-@dp.callback_query(F.data.in_({"lang_en", "lang_ru"}))
+@dp.callback_query(F.data.in_({"en", "ru"}))
 async def set_language(callback_query: types.CallbackQuery, state: FSMContext):
+    chat_id = callback_query.message.chat.id
     user_id = callback_query.from_user.id
-    image_path = os.path.join(os.path.dirname(__file__), "images", 'start_image.jpg')
+    selected_language = callback_query.data
 
-    await bot.delete_message(chat_id=user_id, message_id=callback_query.message.message_id)
+    # Updating the language in the database
+    update_user_language(conn, user_id, selected_language)
 
-    if callback_query.data == "lang_en":
-        user_language[user_id] = "en"
-        await bot.send_message(user_id, "You have selected English.")
-    else:
-        user_language[user_id] = "ru"
-        await bot.send_message(user_id, "Вы выбрали русский язык.")
+    # Updating the language in memory
+    user_language[user_id] = selected_language
 
-    if os.path.exists(image_path):
-        photo = FSInputFile(image_path)
-        await bot.send_photo(user_id, photo=photo)
+    # Deleting a language selection message
+    await bot.delete_message(chat_id=chat_id, message_id=callback_query.message.message_id)
 
-    buttons_en = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Riding Extreme 3D", callback_data="riding_extreme_3d"),
-            InlineKeyboardButton(text="Chain Cube 2048", callback_data="chain_cube_2048")],
-        [InlineKeyboardButton(text="My Clone Army", callback_data="my_clone_army"),
-           InlineKeyboardButton(text="Train Miner", callback_data="train_miner")],
-        [InlineKeyboardButton(text="Merge Away", callback_data="merge_away"),
-            InlineKeyboardButton(text="Twerk Race 3D", callback_data="twerk_race_3d")],
-    ])
+    # Sending confirmation and proceeding to game selection
+    await bot.send_message(
+        chat_id,
+        "You have selected English." if selected_language == "en" else "Вы выбрали русский язык."
+    )
 
-    buttons_ru = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Riding Extreme 3D", callback_data="riding_extreme_3d"),
-            InlineKeyboardButton(text="Chain Cube", callback_data="chain_cube_2048")],
-        [InlineKeyboardButton(text="My Clone Army", callback_data="my_clone_army"),
-            InlineKeyboardButton(text="Train Miner", callback_data="train_miner")],
-        [InlineKeyboardButton(text="Merge Away", callback_data="merge_away"),
-            InlineKeyboardButton(text="Twerk Race 3D", callback_data="twerk_race_3d")],
-    ])
-
-    buttons = buttons_en if user_language[user_id] == "en" else buttons_ru
-    await bot.send_message(user_id, "Choose a game:" if user_language[
-                                                                              user_id] == "en" else "Выберите игру:",
-                           reply_markup=buttons)
-
-    await state.clear()
+    # Switching to game selection
+    await show_game_selection(callback_query.message, selected_language)
 
 
 async def main():
     await dp.start_polling(bot)
     await set_commands(bot)
-
 
 if __name__ == '__main__':
     import asyncio
--- bot/database.py
+++ bot/database.py
@@ -1,6 +1,5 @@
 import os
 import psycopg2
-from psycopg2 import sql
 
 from dotenv import load_dotenv
 
@@ -28,19 +27,41 @@ def create_database_connection():
 
 # Creates the users table if it does not exist.
 def create_table_users(conn):
-    table_name = "users"
     cursor = conn.cursor()
-    cursor.execute(sql.SQL('''
-        CREATE TABLE IF NOT EXISTS {} (
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             chat_id BIGINT NOT NULL UNIQUE,
             user_id BIGINT NOT NULL UNIQUE,
-            first_name VARChAR(100),
+            first_name VARCHAR(100),
             last_name VARCHAR(100),
             username VARCHAR(100),
-            language VARCHAR(10),
-            last_generated_data TIMESTAMPTZ DEFAULT NOW()
+            language_code VARCHAR(10),
+            last_generated_data TIMESTAMPTZ DEFAULT NOW(),
             total_keys_generated INTEGER DEFAULT 0
         )
-    ''').format(sql.Identifier(table_name)))
+    ''')
     conn.commit()
+    cursor.close()
+
+
+# Added new user to the database
+def add_user(conn, chat_id, user_id, first_name, last_name, username, language_code):
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO users (chat_id, user_id, first_name, last_name, username, language_code)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ON CONFLICT (user_id) DO NOTHING
+    ''', (chat_id, user_id, first_name, last_name, username, language_code))
+    conn.commit()
+    cursor.close()
+
+
+# Update user language in the database
+def update_user_language(conn, user_id, language_code):
+    cursor = conn.cursor()
+    cursor.execute('''
+        UPDATE users SET language_code = %s WHERE user_id = %s
+    ''', (language_code, user_id))
+    conn.commit()
+    cursor.close()
