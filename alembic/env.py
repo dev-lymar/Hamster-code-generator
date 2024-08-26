32398f9421eb7489d820ad6a5765decfc2f38d31
Lymar Volodymyr <shaxtar11@gmail.com>
2024-08-26 21:55:46 +0200

    Create first initial migration

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from bot.database.models import Base
-from bot.database.database import DATABASE_URL
+from app.models.game_models import Base as AppBase
+from bot.database.models import Base as BotBase
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -25,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = [AppBase.metadata, BotBase.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
